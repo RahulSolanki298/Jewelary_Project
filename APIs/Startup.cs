7ad9db426376b7d52bec3024ef106f322522d6b6
IT KING <rahulsolanki259@gmail.com>
2025-02-28 10:15:15 +0530

    Add New API PRoject

---
+++ APIs/Startup.cs
@@ -0,0 +1,144 @@
+using APIs.Helper;
+using DataAccess.Data;
+using DataAccess.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIs
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDBContext>(options =>
+           options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDBContext>().AddDefaultTokenProviders();
+
+            var appSettingsSection = Configuration.GetSection("APISettings");
+            services.Configure<APISettings>(appSettingsSection);
+
+            services.Configure<MailJetSettings>(Configuration.GetSection("MailJetSettings"));
+
+            var apiSettings = appSettingsSection.Get<APISettings>();
+            var key = Encoding.ASCII.GetBytes(apiSettings.SecretKey);
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateAudience = true,
+                        ValidateIssuer = true,
+                        ValidAudience = apiSettings.ValidAudience,
+                        ValidIssuer = apiSettings.ValidIssuer,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddScoped<IEmailSender, EmailSender>();
+            services.AddCors(o => o.AddPolicy("Jewelary", builder =>
+            {
+                builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+            }));
+
+            services.AddRouting(option => option.LowercaseUrls = true);
+            services.AddControllers().AddJsonOptions(opt => opt.JsonSerializerOptions.PropertyNamingPolicy = null)
+                .AddNewtonsoftJson(opt =>
+                {
+                    opt.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                    opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "APIs", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please Bearer and then token in the field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                   {
+                     new OpenApiSecurityScheme
+                     {
+                       Reference = new OpenApiReference
+                       {
+                         Type = ReferenceType.SecurityScheme,
+                         Id = "Bearer"
+                       }
+                      },
+                      new string[] { }
+                    }
+                });
+            });
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "APIs", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIs v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCors("Jewelary");
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
--- Business/Repository/ProductRepository.cs
+++ Business/Repository/ProductRepository.cs
@@ -19,44 +19,70 @@ namespace Business.Repository
 
         public async Task<IEnumerable<ProductDTO>> GetProductList()
         {
-            var products = new List<ProductDTO>();
-            products = await (from product in _context.Product
-                              join cat in _context.Category on product.CategoryId equals cat.Id
-                              join subcat in _context.SubCategory on product.SubCategoryId equals subcat.Id
-                              join color in await GetColorList() on product.ColorId equals color.Id
-                              join carat in await GetCaratList() on product.CaratId equals carat.Id
-                              join shape in await GetShapeList() on product.ShapeId equals shape.Id
-                              join clarity in await GetClarityList() on product.ClarityId equals clarity.Id
-                              select new ProductDTO
-                              {
-                                  Id = product.Id,
-                                  Title = product.Title,
-                                  CaratId = carat.Id,
-                                  CaratName = carat.Name,
-                                  CategoryId = cat.Id,
-                                  CategoryName = cat.Name,
-                                  ColorId = color.Id,
-                                  ColorName = color.Name,
-                                  SubCategoryId = subcat.Id,
-                                  SubCategoryName = subcat.Name,
-                                  ClarityId = clarity.Id,
-                                  ClarityName = clarity.Name,
-                                  ShapeName = shape.Name,
-                                  ShapeId = shape.Id,
-                                  UnitPrice = product.UnitPrice,
-                                  Price = product.Price,
-                                  IsActivated = product.IsActivated,
-                                  CaratSizeId=product.CaratSizeId,
-                                  CaratSizeName=carat.Name,
-                                  Description=product.Description,
-                                  Sku=product.Sku,
-                                  ProductType=cat.ProductType,
-                                  StyleId=product.StyleId
-                              }).ToListAsync();
+            var colors = await GetColorList();
+            var carats = await GetCaratList();
+            var shapes = await GetShapeList();
+            var clarities = await GetClarityList();
+
+            var products = await (from product in _context.Product
+                                  join cat in _context.Category on product.CategoryId equals cat.Id // INNER JOIN (Category remains unchanged)
+                                  join subcat in _context.SubCategory on product.SubCategoryId equals subcat.Id 
+
+                                  join color in _context.ProductProperty on product.ColorId equals color.Id into colorGroup
+                                  from color in colorGroup.DefaultIfEmpty() // LEFT JOIN
+
+                                  join carat in _context.ProductProperty on product.CaratId equals carat.Id into caratGroup
+                                  from carat in caratGroup.DefaultIfEmpty() // LEFT JOIN
+
+                                  join shape in _context.ProductProperty on product.ShapeId equals shape.Id into shapeGroup
+                                  from shape in shapeGroup.DefaultIfEmpty() // LEFT JOIN
+
+                                  join clarity in _context.ProductProperty on product.ClarityId equals clarity.Id into clarityGroup
+                                  from clarity in clarityGroup.DefaultIfEmpty() // LEFT JOIN
+
+                                  join size in _context.ProductProperty on product.CaratSizeId equals size.Id into sizeGroup
+                                  from size in sizeGroup.DefaultIfEmpty() // LEFT JOIN
+
+                                  join sty in _context.ProductProperty on product.StyleId equals sty.Id into styleGroup
+                                  from sty in styleGroup.DefaultIfEmpty() // LEFT JOIN
+
+                                  join col in _context.CollectionHistory on product.CollectionsId equals col.Id into colGroup
+                                  from col in colGroup.DefaultIfEmpty() // LEFT JOIN
+
+                                  select new ProductDTO
+                                  {
+                                      Id = product.Id,
+                                      Title = product.Title,
+                                      CaratId = carat != null ? carat.Id : (int?)null,
+                                      CaratName = carat != null ? carat.Name : null,
+                                      CategoryId = cat.Id,
+                                      CategoryName = cat.Name,
+                                      ColorId = color != null ? color.Id : (int?)null,
+                                      ColorName = color != null ? color.Name : null,
+                                      SubCategoryId = subcat != null ? subcat.Id : (int?)null,
+                                      SubCategoryName = subcat != null ? subcat.Name : null,
+                                      ClarityId = clarity != null ? clarity.Id : (int?)null,
+                                      ClarityName = clarity != null ? clarity.Name : null,
+                                      ShapeName = shape != null ? shape.Name : null,
+                                      ShapeId = shape != null ? shape.Id : (int?)null,
+                                      UnitPrice = product.UnitPrice,
+                                      Price = product.Price,
+                                      IsActivated = product.IsActivated,
+                                      CaratSizeId = product.CaratSizeId,
+                                      CaratSizeName = size != null ? size.Name : null,
+                                      Description = product.Description,
+                                      Sku = product.Sku,
+                                      ProductType = cat.ProductType,
+                                      StyleId = product.StyleId,
+                                      StyleName = sty != null ? sty.Name : null,
+                                      CollectionsId = product.CollectionsId,
+                                      CollectionName = col != null ? col.HistoryTitle : null
+                                  }).ToListAsync();
 
             return products;
 
         }
+
 
         public async Task<int> GetColorId()
         {
@@ -109,6 +135,8 @@ namespace Business.Repository
             var result = await _context.ProductProperty.Where(x => x.ParentId == clarityId).ToListAsync();
             return result;
         }
+
+
 
     }
 }
