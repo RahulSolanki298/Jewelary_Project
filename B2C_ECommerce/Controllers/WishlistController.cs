2eb5d3dc5e75dc2feebe9dc95bf1696f915387fe
IT KING <rahulsolanki259@gmail.com>
2025-04-26 08:44:31 +0530

    Optimize the code

--- B2C_ECommerce/Controllers/WishlistController.cs
+++ B2C_ECommerce/Controllers/WishlistController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using B2C_ECommerce.IServices;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace B2C_ECommerce.Controllers
 {
     public class WishlistController : Controller
     {
+        private readonly IDiamondService _diamondService;
+
+        public WishlistController(IDiamondService diamondService)
+        {
+                _diamondService = diamondService;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetWishListByIds(string diamondIds)
+        {
+            int[] diamondIdArray = diamondIds.Split(',')
+                                  .Select(id => int.Parse(id))
+                                  .ToArray();
+            var response = await _diamondService.GetSelectedDiamondByIds(diamondIdArray);
+            return PartialView("~/Views/Wishlist/_WishlistDiamonds.cshtml", response);
         }
     }
 }
