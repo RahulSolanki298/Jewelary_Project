8a1eed29594ab0a77650eece448cf7cd204a42be
ASROCK <ASROCK@DESKTOP-HELUM8S>
2025-05-06 12:39:27 +0530

    optimiz the data

---
+++ B2C_ECommerce/Controllers/AddToCardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace B2C_ECommerce.Controllers
+{
+    public class AddToCardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
