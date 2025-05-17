210f7beb82c390c2ddcff2cb547681b45b6ed97a
IT KING <rahulsolanki259@gmail.com>
2025-05-17 12:53:32 +0530

    Update data

--- ControlPanel/Controllers/DiamondController.cs
+++ ControlPanel/Controllers/DiamondController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace ControlPanel.Controllers
 {
     public class DiamondController : Controller
     {
+        private readonly IDiamondRepository _diamondRepository;
+
+        public DiamondController(IDiamondRepository diamondRepository)
+        {
+                _diamondRepository= diamondRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DiamondList()
+        {
+            var data= await _diamondRepository.GetDiamondList();
+            return PartialView("_DiamondList", data);
         }
     }
 }
