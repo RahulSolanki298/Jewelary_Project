cb670eb641b457881a984815ca81817f484d6eec
ASROCK <ASROCK@DESKTOP-HELUM8S>
2025-06-04 16:09:14 +0530

    Modify the data

--- B2C_ECommerce/Startup.cs
+++ B2C_ECommerce/Startup.cs
@@ -13,7 +13,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 
 namespace B2C_ECommerce
